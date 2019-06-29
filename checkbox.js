ca07d4c1f9850217f006021c139a2cd78a6d9129
임형묵 <skygudanr@naver.com>
2019-06-29 14:41:00 +0900

    Add files via upload

---
+++ checkbox.js
@@ -0,0 +1,30 @@
+function checkbox(elem) {
+
+	var el = elem || 'input[type=checkbox]';
+
+	var key;
+
+	$(window).keydown(function(e) {
+		key = e.keyCode || e.which;
+	})
+
+
+	$(el).focus(function() {
+		if (key == 9) {
+			$(this).parents('label').addClass('focus');
+		}
+		key = null;
+	})
+
+	$(el).blur(function() {
+		$(this).parents('label').removeClass('focus');
+	})
+
+	$(el).change(function() {
+		if ($(this).is(':checked')) {
+			$(this).parents('label').addClass('active');
+		} else {
+			$(this).parents('label').removeClass('active');
+		}
+	});
+}
