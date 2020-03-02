8afc26f78b174de98617ea167527d677f1119a97
임형묵 <skygudanr@naver.com>
2020-03-03 04:46:42 +0900

    Update checkbox.js

--- checkbox.js
+++ checkbox.js
@@ -1,28 +1,28 @@
 function checkbox(elem) {
 
-	var el = elem || 'input[type=checkbox]';
-	var key;
+    var el = elem || 'input[type=checkbox]';
+    var key;
 
-	$(window).keydown(function(e) {
-		key = e.keyCode || e.which;
-	})
+    $(window).keydown(function(e) {
+        key = e.keyCode || e.which;
+    })
 
-	$(el).focus(function() {
-		if (key == 9) {
-			$(this).parents('label').addClass('focus');
-		}
-		key = null;
-	})
+    $(el).focus(function() {
+        if (key == 9) {
+            $(this).parents('label').addClass('focus');
+        }
+        key = null;
+    })
 
-	$(el).blur(function() {
-		$(this).parents('label').removeClass('focus');
-	})
+    $(el).blur(function() {
+        $(this).parents('label').removeClass('focus');
+    })
 
-	$(el).change(function() {
-		if ($(this).is(':checked')) {
-			$(this).parents('label').addClass('active');
-		} else {
-			$(this).parents('label').removeClass('active');
-		}
-	});
+    $(el).change(function() {
+        if ($(this).is(':checked')) {
+            $(this).parents('label').addClass('active');
+        } else {
+            $(this).parents('label').removeClass('active');
+        }
+    });
 }
